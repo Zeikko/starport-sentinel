a3a397b5e074d463b9ecdc4350e113f30aa590f4
Jaakko Ojalehto <jaakko@ojalehto.fi>
2024-11-06 20:02:22 +0200

    Add a single random security rule

--- source/game/game.gd
+++ source/game/game.gd
@@ -1,9 +1,12 @@
 extends Node
 
 var ship_scene: PackedScene = preload('res://source/ship/ship.tscn')
+var security_rules: Array[Dictionary] = []
 
 func _ready() -> void:
 	create_ship()
+	security_rules.push_back(create_security_rule())
+	Ui.update_security_briefing()
 
 func create_ship() -> void:
 	var ship: Ship = ship_scene.instantiate()
@@ -13,3 +16,14 @@ func create_ship() -> void:
 
 func _on_timer_timeout() -> void:
 	create_ship()
+
+func create_security_rule() -> Dictionary:
+	var type = Ship.Type.values().pick_random()
+	var faction = Ship.Faction.values().pick_random()
+	var rule_variation = randi_range(0, 1)
+	var security_rule = {}
+	if rule_variation == 0:
+		security_rule.type = type
+	if rule_variation == 1:
+		security_rule.faction = faction
+	return security_rule
--- source/ui/ui.gd
+++ source/ui/ui.gd
@@ -10,15 +10,33 @@ var selected_ship: Ship:
 @onready var ship_name: Label = %ShipName
 @onready var status: Label = %Status
 @onready var faction_and_class: Label = %FactionAndClass
+@onready var security_rules: VBoxContainer = %SecurityRules
 
 func update_ship_details() -> void:
 	ship_name.set_text(selected_ship.ship_name)
 	status.set_text(Ship.Status.find_key(selected_ship.status).capitalize())
 	faction_and_class.set_text(
-		Ship.Faction.find_key(selected_ship.faction).capitalize()
-		+ ' ' +
+		Ship.Faction.find_key(selected_ship.faction).capitalize() + ' ' +
 		Ship.Type.find_key(selected_ship.type).capitalize()
 	)
+
+
+func update_security_briefing() -> void:
+	for child in security_rules.get_children():
+		security_rules.remove_child(child)
+		child.free()
+	for security_rule in Game.security_rules:
+		var label = Label.new()
+		var text = ''
+		var faction = security_rule.get('faction')
+		var type = security_rule.get('type')
+		if faction:
+			text += Ship.Faction.find_key(faction).capitalize() + ' '
+		if type:
+			text += Ship.Type.find_key(type).capitalize() + ' '
+		text += 'is not allowed'
+		label.set_text(text)
+		security_rules.add_child(label)
 
 
 func _on_approve_button_pressed() -> void:
