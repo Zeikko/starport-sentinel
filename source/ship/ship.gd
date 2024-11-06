70e9a16ea25929aaee8acf55b1c5f40cadfb2667
Jaakko Ojalehto <jaakko@ojalehto.fi>
2024-11-06 19:14:23 +0200

    Add ship class and faction ship information

--- source/ship/ship.gd
+++ source/ship/ship.gd
@@ -1,8 +1,11 @@
 class_name Ship extends Node2D
 
 enum Status { UNDECIDED, APPROVED, REJECTED }
+enum Type { SHUTTLE, FRIGATE, CRUISER }
+enum Faction { VOID, OBUDU, ARGUS }
 var max_distance: float = 300
 var min_distance: float = 40
+var scale_multiplier: float = 0.1
 
 var distance: float = max_distance
 var angle: float = 0
@@ -17,9 +20,22 @@ var status: Status = Status.UNDECIDED:
 			modulate = Color.CRIMSON
 			speed = abs(speed) * -1
 		status = value
-		Ui.update_ship_details()
+		if Ui.selected_ship == self:
+			Ui.update_ship_details()
 	get:
 		return status
+var type: Type:
+	set (value):
+		scale = Vector2(value + 1, value + 1) * scale_multiplier
+		type = value
+	get:
+		return type
+var faction: Faction
+
+func _ready() -> void:
+	ship_name = str(randi())
+	type = Type.values().pick_random()
+	faction = Faction.values().pick_random()
 
 func _process(delta: float) -> void:
 	if distance < min_distance:
--- source/ui/ui.gd
+++ source/ui/ui.gd
@@ -9,16 +9,12 @@ var selected_ship: Ship:
 @onready var radar: Node2D = %Radar
 @onready var ship_name: Label = %ShipName
 @onready var status: Label = %Status
-
+@onready var faction_and_class: Label = %FactionAndClass
 
 func update_ship_details() -> void:
 	ship_name.set_text(selected_ship.ship_name)
-	if selected_ship.status == Ship.Status.APPROVED:
-		status.set_text('Approved')
-	elif selected_ship.status == Ship.Status.REJECTED:
-		status.set_text('Rejected')
-	else:
-		status.set_text('Undecided')
+	status.set_text(Ship.Status.find_key(selected_ship.status).capitalize())
+	faction_and_class.set_text(Ship.Faction.find_key(selected_ship.faction).capitalize() + ' ' + Ship.Type.find_key(selected_ship.type).capitalize())
 
 
 func _on_approve_button_pressed() -> void:
