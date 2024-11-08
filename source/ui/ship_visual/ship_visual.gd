cc55a551a333a28e2572d469ce827bc5709165eb
Jaakko Ojalehto <jaakko@ojalehto.fi>
2024-11-09 00:38:44 +0200

    Lint

--- source/ui/ship_visual/ship_visual.gd
+++ source/ui/ship_visual/ship_visual.gd
@@ -1,12 +1,11 @@
 extends Control
 
-@onready var faction_logo: TileMapLayer = %FactionLogo
-
 var faction_map: Dictionary = {
 	Ship.Faction.VOID: Vector2i(0, 0),
 	Ship.Faction.ARGUS: Vector2i(2, 0),
 	Ship.Faction.OBUDU: Vector2i(3, 1),
 }
+@onready var faction_logo: TileMapLayer = %FactionLogo
 
 func update() -> void:
 	faction_logo.set_cell(Vector2i(0, 0), 0, faction_map[Ui.selected_ship.faction])
