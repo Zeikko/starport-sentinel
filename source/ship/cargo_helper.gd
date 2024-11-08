06eb05a41e905b2168e8d6e01c4efd42cf92cdf8
Yilun Sun <yilunsun11@yeah.net>
2024-11-08 09:52:29 +0800

    Improve code readability

--- source/ship/cargo_helper.gd
+++ source/ship/cargo_helper.gd
@@ -7,13 +7,13 @@ func generate_cargo_info(ship: Ship) -> Dictionary:
 		"Metals", "Metawheat", "Artwork", "Machinery", "Tools", "Building Material",
 		"Junk", "Slaves", "Small Arms", "Food Products", "Grav Tanks", "Ammunition", "Batteries"
 	]
-	var cargo_holds: int = int(ship.type) + 1
-	for i in range(cargo_holds):
+	var cargo_hold_number: int = int(ship.type) + 1
+	for cargo_hold_index in range(cargo_hold_number):
 		var cargo_hold: Dictionary = {}
-		var num_items: int = randi() % 5 + 1
-		for j in range(num_items):
-			var item: String = cargo_items[randi() % cargo_items.size()]
-			var quantity: int = randi() % 100 + 1
+		var num_items: int = randi_range(1, 6)
+		for item_number in range(num_items):
+			var item: String = cargo_items.pick_random()
+			var quantity: int = randi_range(1, 101)
 			cargo_hold[item] = quantity
-		cargo_info["Cargo Hold " + str(i + 1)] = cargo_hold
+		cargo_info["Cargo Hold " + str(cargo_hold_index + 1)] = cargo_hold
 	return cargo_info
