c58ac1109dd02160c2e1c3b5f6dfc6bd9bcf34e4
Jaakko Ojalehto <jaakko@ojalehto.fi>
2024-11-27 20:35:03 +0200

    Add hover sound

--- source/ui/effects/ui_sounds.gd
+++ source/ui/effects/ui_sounds.gd
@@ -19,7 +19,11 @@ func _on_node_added(node:Node) -> void:
 	if node is Button:
 		# If the added node is a button we connect to its mouse_entered and pressed signals
 		# and play a sound
+		node.mouse_entered.connect(_play_hover)
 		node.pressed.connect(_play_pressed)
+
+func _play_hover() -> void:
+	playback.play_stream(preload("res://ui/effects/hover.wav"), 0,-6, randf_range(0.9, 1.1), 0, "Effects")
 
 func _play_pressed() -> void:
 	playback.play_stream(preload("res://ui/effects/click.wav"), 0,-6, randf_range(0.9, 1.1), 0, "Effects")
