7b5f3da5c96895b9377ce72c4cb624c1e14c1f48
Woffelson <twingelitwongeli@hotmail.com>
2024-11-30 18:57:20 +0200

    variable name

--- source/ui/effects/scanlight.gd
+++ source/ui/effects/scanlight.gd
@@ -1,9 +1,9 @@
 extends Panel
 
 func _ready() -> void:
-	var matsku: ShaderMaterial = material
-	matsku.set_shader_parameter("lightning_color",[Color("ee8695"), Color("4a7a96")].pick_random())
-	matsku.set_shader_parameter("size",randf_range(0.001,0.02))
-	matsku.set_shader_parameter("width",randf_range(0.25,1.0))
-	matsku.set_shader_parameter("speed",randf_range(2.0,10.0))
-	matsku.set_shader_parameter("cycle",randf_range(0.2,5.0))
+	var shader_material: ShaderMaterial = material
+	shader_material.set_shader_parameter("lightning_color",[Color("ee8695"), Color("4a7a96")].pick_random())
+	shader_material.set_shader_parameter("size",randf_range(0.001,0.02))
+	shader_material.set_shader_parameter("width",randf_range(0.25,1.0))
+	shader_material.set_shader_parameter("speed",randf_range(2.0,10.0))
+	shader_material.set_shader_parameter("cycle",randf_range(0.2,5.0))
