7755041c5eb04a52dd3ec7543d83a4cfd4e580d5
Woffelson <twingelitwongeli@hotmail.com>
2024-11-30 19:15:04 +0200

    random fix

--- source/ui/effects/scanlight.gd
+++ source/ui/effects/scanlight.gd
@@ -2,7 +2,8 @@ extends Panel
 
 func _ready() -> void:
 	var shader_material: ShaderMaterial = material
-	shader_material.set_shader_parameter("lightning_color",[Color("ee8695"), Color("4a7a96")].pick_random())
+	var random_color = [Color("ee8695"), Color("4a7a96")].pick_random()
+	shader_material.set_shader_parameter("lightning_color", random_color)
 	shader_material.set_shader_parameter("size",randf_range(0.001,0.02))
 	shader_material.set_shader_parameter("width",randf_range(0.25,1.0))
 	shader_material.set_shader_parameter("speed",randf_range(2.0,10.0))
