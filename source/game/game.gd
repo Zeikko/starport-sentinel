796ab07ae9ad8d3b50d0b2e27f6de286a6c6b27d
Jaakko Ojalehto <jaakko@ojalehto.fi>
2024-12-01 15:28:02 +0200

    Add more upgrades

--- source/game/game.gd
+++ source/game/game.gd
@@ -8,9 +8,11 @@ const UPGRADES_PATH: String = "res://shift/upgrades/"
 
 var upgrades: Dictionary = {
 	Upgrade.Id.SCANNER_SPEED: preload(UPGRADES_PATH + "scanner_speed_up.tres"),
-	Upgrade.Id.ARMOR: preload(UPGRADES_PATH + "armor_up.tres")
+	Upgrade.Id.ARMOR: preload(UPGRADES_PATH + "armor_up.tres"),
+	Upgrade.Id.REPAIR_BOTS: preload(UPGRADES_PATH + "repair_bots.tres"),
+	Upgrade.Id.SHIELD_GENERATOR: preload(UPGRADES_PATH + "shield_generator.tres")
 }
-var armor: int = 0
+var shield: int = 0
 var hit_points: int = 100:
 	set(value):
 		if value <= 0 && hit_points > 0:
@@ -61,8 +63,8 @@ func _on_cheatcode_entered(code: Cheatcode) -> void:
 				ship.status = Ship.Status.APPROVED
 
 func shift_ended() -> void:
-	if upgrades[Upgrade.Id.ARMOR].bought:
-		armor = 1
+	if upgrades[Upgrade.Id.SHIELD_GENERATOR].bought:
+		shield = 1
 
 func get_ships() -> Array[Ship]:
 	var ships: Array[Ship] = []
@@ -75,8 +77,8 @@ func upgrade_bought(id: Upgrade.Id) -> void:
 	if !upgrades[id].bought:
 		upgrades[id].bought = true
 		match id:
-			Upgrade.Id.ARMOR:
-				armor = 1
+			Upgrade.Id.SHIELD_GENERATOR:
+				shield = 1
 			Upgrade.Id.SCANNER_SPEED:
 				scanning_speed = base_scanning_speed * 2
 
